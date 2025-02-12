c48a114d7f87f1685bc18e18a79668f7f2513c0e
Siraj Qazi <siraj.qazi@seeloz.com>
2025-02-12 05:16:38 +0500

    feat(logger.py): add support for logging to both a file and the console with different log levels and messages

--- src/logger.py
+++ src/logger.py
@@ -12,27 +12,57 @@ class BotLogger:
         self.logger = logging.getLogger('RedditBot')
         self.logger.setLevel(logging.INFO)
         
-        # Create formatters and handlers
-        formatter = logging.Formatter(
+        # Create formatters
+        file_formatter = logging.Formatter(
             '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
         )
+        console_formatter = logging.Formatter(
+            '%(asctime)s - %(levelname)s - %(message)s'
+        )
         
-        # File handler
+        # File handler - gets full messages
         file_handler = logging.FileHandler(
             f'logs/reddit_bot_{datetime.now().strftime("%Y%m%d")}.log'
         )
-        file_handler.setFormatter(formatter)
+        file_handler.setFormatter(file_formatter)
+        file_handler.setLevel(logging.INFO)
         
-        # Console handler
+        # Console handler - gets truncated messages
         console_handler = logging.StreamHandler()
-        console_handler.setFormatter(formatter)
+        console_handler.setFormatter(console_formatter)
+        console_handler.setLevel(logging.INFO)
         
         # Add handlers to logger
         self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
-    
-    def info(self, message):
-        self.logger.info(message)
+
+    def _log_dual(self, level, full_message, console_message=None):
+        """Log different messages to file and console"""
+        # Get the current logger
+        logger = logging.getLogger('RedditBot')
+        
+        # If no console message is provided, use the full message
+        console_message = console_message or full_message
+        
+        # Store the original handlers
+        handlers = logger.handlers[:]
+        
+        # Log full message to file
+        for handler in logger.handlers:
+            if isinstance(handler, logging.FileHandler):
+                logger.callHandlers(logging.LogRecord(
+                    'RedditBot', level, '', 0, full_message, (), None
+                ))
+        
+        # Log truncated message to console
+        for handler in logger.handlers:
+            if isinstance(handler, logging.StreamHandler) and not isinstance(handler, logging.FileHandler):
+                logger.callHandlers(logging.LogRecord(
+                    'RedditBot', level, '', 0, console_message, (), None
+                ))
+
+    def info(self, message, console_message=None):
+        self._log_dual(logging.INFO, message, console_message)
         
     def error(self, message, exc_info=True):
         self.logger.error(message, exc_info=exc_info)
@@ -40,5 +70,5 @@ class BotLogger:
     def warning(self, message):
         self.logger.warning(message)
         
-    def debug(self, message):
-        self.logger.debug(message) 
+    def debug(self, message, console_message=None):
+        self._log_dual(logging.DEBUG, message, console_message) 
